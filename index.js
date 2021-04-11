b80d70b39ff7280fb7b595e07a118f90b2ad9247
Candice Peters <candice.peters@flatironschool.com>
2021-04-11 18:54:59 -0500

    commit tree 6b5325e22280186a12e00e92a27105cd6cd8d6e9

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+ 
+  } 
+
+function moveDodgerRight(){
+    const rightNumbers = dodger.style.left.replace("px", "");
+    //look at what css looks like and why you need to remove the px and turn the string to a number
+    const right = parseInt(rightNumbers, 10);
+    if (right > 0) {
+      dodger.style.left = `${right + 1}px`;
+    }
+   
+}
+document.addEventListener("keydown", function(e) {
+    let makers = "Hello"
+console.log(makers)
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key ==="ArrowRight"){
+        moveDodgerRight();
+    }
+  });
+
